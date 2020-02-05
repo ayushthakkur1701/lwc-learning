88dba39f678962916b012500a8775808a7a86c9e
ayushthakkur1701 <ayushthakur1701@gmail.com>
2020-02-05 17:48:23 +0530

    changes in App.js for validation rules

--- src/modules/my/app/app.js
+++ src/modules/my/app/app.js
@@ -3,36 +3,85 @@
 import { LightningElement,track,api} from 'lwc';
 
 
-let loginEmail,loginPassword;
-let loginTempId;
+//let loginEmail,loginPassword;
+//let loginTempId;
+const isEmail = (email = null) => {
+  
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+     return regex.test(email);
+     
+}
 export default class App extends LightningElement {
     @track showDash = false;
     @track showLogin = true;
     @track chlidComponentHeading='';
+    @track loginEmail='';
+    @track loginPassword='';
+
         handleName(event){
 
-            loginEmail = event.target.value;
+            this.loginEmail = event.target.value;
+            
         }
 
         handlePassword(event){
 
-            loginPassword = event.target.value;
+            this.loginPassword = event.target.value;
         }
 
         
+        
         handleSubmit(){
             //this.showDash = true;
-            this.showLogin = false;
-            //alert(loginEmail+''+loginPassword);
-
-            //loginTempId = this.template.querySelector('div').chidNodes;
-            //customElements.define('my-dash-board', buildCustomElementConstructor(DashBoard));
-            //console.log(loginTempId);
-            
-            // loginTempId.style.display = "none";
-            this.chlidComponentHeading = 'Dash Board';
-              
+            //const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            let re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+            let paswd=  /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+            if(this.loginEmail === "") {
+                alert("Error: Email cannot be blank!");
+                this.loginEmail='';
+               
+              }
+              else if(this.loginEmail !== "") {
+                re = /^\w+$/;
+                this.loginEmail='';
+                if(!re.test(this.loginEmail)){
+                    alert("Error: Email must contain @ and . ");
+                }
+   
+                
+              }
+          
+              if(this.loginPassword !== "" ) {
+                if(this.loginPassword.length < 6) {
+                    this.loginEmail='';
+                    this.loginPassword='';
+                  alert("Error: Password must contain at least six characters!");
+                  
+                }
+                if(this.loginPassword === this.loginEmail) {
+                  alert("Error: Password must be different from Username!");
+                  
+                }else{
+                    
+                    if(this.loginPassword.match(paswd)) 
+                    { 
+                      alert('Password must be between 7 to 15 characters which contain at least one numeric digit and a special character');  
+                    }
+                
+                }
+              } 
+              else {
+                alert("Error: Please check that you've entered and confirmed your password!");
+                return false;
+              }
+              this.showLogin = false;
+              this.chlidComponentHeading = 'Dash Board';
+              return true;
         }
+
+        
+
 
         
 
